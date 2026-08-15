ba6df011ee09b34708f441e437b4016e2e85b74b
limbj <buhmjee@naver.com>
2026-08-15 19:47:48 +0900

    feat: 社員別の社会保険加入情報照会を追加
    - 社員別の保険加入情報を保持するモデルを追加
    - insuranceテーブルから加入保険と基準報酬額を取得するDAOを追加
    - 保険金額が未入力の場合を0として扱うように対応

--- src/main/java/employee/dao/InsuranceDao.java
+++ src/main/java/employee/dao/InsuranceDao.java
@@ -1,5 +1,47 @@
 package employee.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import employee.model.EmployeeInsurance;
+
+// 사원별 보험 가입정보 조회
 public class InsuranceDao {
 
+	public List<EmployeeInsurance> selectByEmployeeId(
+		Connection conn,
+		Integer employeeId)
+		throws SQLException {
+
+		String sql = "SELECT insurance_agency, "
+			+ "NVL(insurance_amount, 0) AS insurance_amount "
+			+ "FROM insurance "
+			+ "WHERE employee_id = ? "
+			+ "ORDER BY insurance_id";
+
+		List<EmployeeInsurance> result = new ArrayList<>();
+
+		try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+			pstmt.setInt(1, employeeId);
+
+			try (ResultSet rs = pstmt.executeQuery()) {
+
+				while (rs.next()) {
+
+					EmployeeInsurance insurance = new EmployeeInsurance(
+						rs.getString("insurance_agency"),
+						rs.getLong("insurance_amount"));
+
+					result.add(insurance);
+				}
+			}
+		}
+
+		return result;
+	}
 }
